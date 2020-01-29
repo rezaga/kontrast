6f3d7bcaa17ec74df8f42c6e66e47ac77e1d166e
rezaga <58208290+rezaga@users.noreply.github.com>
2020-01-29 14:34:31 +0100

    Update main.js

--- main.js
+++ main.js
@@ -18,6 +18,9 @@ const onCalcContrast = () => {
     let el = document.getElementById("result-text")
     if (weight < 5 ) {
         el.innerHTML = "konferere med din radiolog om mængden af kontrast"
+    if (weight == "") {
+        el.innerHTML = "Skriv en vægt for beregning"
+    }
     } else {
         el.innerHTML = contastAmount + ' ml Gadovist iv.'
     }
