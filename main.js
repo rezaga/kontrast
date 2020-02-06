17ab655c21ba151e1c6a62ee05b94bee4efc7f99
rezaga <58208290+rezaga@users.noreply.github.com>
2020-02-06 14:16:39 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,24 +5,37 @@ const calcContrast = weight => {
     } else if (weight >= 5 && weight < 63) {
         return 5
     } else if (weight >= 63 && weight < 75) {
-        return weight / 10
+        return weight / 10.0
     } else {
         return 7.5
+    }
+}
+const calcContrastPrim = weight => {
+    if (weight < 5) {
+        return null
+    } else if (weight < 100) {
+        return weight / 10.0
+    } else {
+        return 10
     }
 }
 
 // function called on button click event
 const onCalcContrast = () => {
     let weight = document.getElementById("weight-input").value
-    let contastAmount = calcContrast(weight)
+    let contrastAmount = calcContrast(weight)
+    let contrastAmountPrim = calcContrastPrim(weight)
     let el = document.getElementById("result-text")
-    if (weight < 5) {
-        el.innerHTML = "konferere med din radiolog om mængden af kontrast"
     if (weight == "") {
         el.innerHTML = "Skriv en vægt for beregning"
-    }
+    } else if (weight < 5) {
+        el.innerHTML = "konferere med din radiolog om mængden af kontrast"
+    } else if (selector.options[selector.selectedIndex].value == "Primovist") {
+        el.innerHTML = weight + " kg. = " + contrastAmountPrim + "ml Primovist iv."
+    } else if (selector.options[selector.selectedIndex].value == "Gadovist") {
+        el.innerHTML = weight + " kg. = " + contrastAmount + " ml Gadovist iv."
     } else {
-        el.innerHTML = weight + ' kg. = ' + contastAmount + ' ml Gadovist iv.';
+            el.innerHTML = "konferere med din radiolog om mængden af kontrast"
     }
 }
 window.onload = function() {
