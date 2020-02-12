23fa279e34735fc5bc138a596630315fdf2c9ae6
rezaga <58208290+rezaga@users.noreply.github.com>
2020-02-12 09:25:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -10,6 +10,19 @@ const calcContrast = weight => {
         return 7.5
     }
 }
+
+const calcContrastDobble = weight => {
+    if (weight < 5) {
+        return null
+    } else if (weight >= 5 && weight < 63) {
+        return (weight / 10.0) * 2
+    } else if (weight >= 63 && weight < 75) {
+        return 12.5
+    } else {
+        return 15
+    }
+}
+
 const calcContrastPrim = weight => {
     if (weight < 5) {
         return null
@@ -25,17 +38,21 @@ const onCalcContrast = () => {
     let weight = document.getElementById("weight-input").value
     let contrastAmount = calcContrast(weight)
     let contrastAmountPrim = calcContrastPrim(weight)
+    let contrastAmountDob = calcContrastDobble(weight)
     let el = document.getElementById("result-text")
+    let chk = document.getElementById("s1").checked
     if (weight == "") {
         el.innerHTML = "Skriv en vægt for beregning"
     } else if (weight < 5) {
         el.innerHTML = "konferere med din radiolog om mængden af kontrast"
     } else if (selector.options[selector.selectedIndex].value == "Primovist") {
         el.innerHTML = weight + " kg. =  " + contrastAmountPrim + " ml Primovist iv."
+    } else if (chk == true) {
+        el.innerHTML = weight + " kg. = " + contrastAmountDob + " ml Gadovist iv." 
     } else if (selector.options[selector.selectedIndex].value == "Gadovist") {
-        el.innerHTML = weight + " kg. =  " + contrastAmount + " ml Gadovist iv."
+        el.innerHTML = weight + " kg. =  " + contrastAmount + " ml Gadovist iv."       
     } else {
-            el.innerHTML = "konferere med din radiolog om mængden af kontrast"
+        el.innerHTML = "konferere med din radiolog om mængden af kontrast"
     }
 }
 window.onload = function() {
@@ -45,7 +62,17 @@ window.onload = function() {
        }
     }
 }
-function clearInputField() {
+const clearInputField = () => {
     document.getElementById("weight-input").value = "";
-    
+    document.getElementById("s1").checked = false;  
 }
+const disCheckBox = () => {
+    if (selector.options[selector.selectedIndex].value == "Primovist") {
+        document.getElementById("s1").hidden = true;
+        document.getElementById("labs1").hidden = true;
+    } else {
+        document.getElementById("s1").hidden = false;
+        document.getElementById("labs1").hidden = false;
+    }
+}
+
